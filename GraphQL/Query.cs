a89ad8a5dab37f4e3c2b25ad8b059005e7e659a8
alikemalbilgili <bilgilialikemal56@gmail.com>
2023-06-11 18:21:13 +0300

    Postgress connected graphql added and platform query created

---
+++ GraphQL/Query.cs
@@ -0,0 +1,14 @@
+using CommanderGQL.Data;
+using CommanderGQL.Models;
+
+namespace CommanderGQL.GraphQL
+{
+    public class Query
+    {
+        public IQueryable<Platform> GetPlatforms([Service] AppDbContext context)
+        {
+            return context.Platforms;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
+using CommanderGQL.Data;
+using CommanderGQL.GraphQL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(config.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGraphQL();
+
+// app.UseEndpoints(endpoints =>
+// {
+//     endpoints.MapGraphQL();
+// });
+
+
 
 app.Run();
